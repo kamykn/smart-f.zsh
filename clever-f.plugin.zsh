e12677f26fea70ddeedfd9dc1993ce5bb4dbe90e
kamykn <to.kamykn@gmail.com>
2019-07-19 23:46:25 +0900

    switch searching direction

--- clever-f.plugin.zsh
+++ clever-f.plugin.zsh
@@ -1,5 +1,6 @@
 local search_type_next=1
 local search_type_prev=2
+local search_type_mode=0
 
 cleverf() {
 	local search_type=$1
@@ -24,19 +25,20 @@ cleverf() {
 }
 
 cleverf-vi-find() {
-local search_type=$1
-local tmp_prev_cursor_pos=$2
-local current_cursor_pos=${CURSOR}
+	local search_type=$1
+	local tmp_prev_cursor_pos=$2
+	local current_cursor_pos=${CURSOR}
 
 	# repeatじゃない場合
-	if [[ ${tmp_prev_cursor_pos} -ne ${current_cursor_pos} ]]; then
-		if [[ search_type -eq search_type_next ]]; then
+	if [[ $tmp_prev_cursor_pos -ne $current_cursor_pos ]]; then
+		if [[ $search_type -eq $search_type_next ]]; then
 			zle .vi-find-next-char
 		else
 			zle .vi-find-prev-char
 		fi
 
 		if [[ $? -eq 0 ]]; then
+			search_type_mode=$search_type
 			return 0
 		fi
 	fi
@@ -44,12 +46,17 @@ local current_cursor_pos=${CURSOR}
 	local is_current_line=true
 	for line in {1..$BUFFERLINES}; do
 		# repeatマッチ
-		zle .vi-repeat-find
+		if [[ $search_type_mode -eq $search_type ]]; then
+			zle .vi-repeat-find
+		else
+			# repeatを逆向きにする用
+			zle .vi-rev-repeat-find
+		fi
 
 		if [[ $? -eq 0 ]]; then
 			if ! "${is_current_line}"; then
 				# 次/前の行マッチの1文字目がヒットできないので戻す
-				if [[ search_type -eq search_type_next ]]; then
+				if [[ $search_type -eq $search_type_next ]]; then
 					zle .vi-rev-repeat-find
 				else
 					zle .vi-repeat-find
@@ -60,7 +67,7 @@ local current_cursor_pos=${CURSOR}
 		fi
 
 		# 行移動
-		if [[ search_type -eq search_type_next ]]; then
+		if [[ $search_type -eq $search_type_next ]]; then
 			cleverf-move-next-line
 		else
 			cleverf-move-prev-line
@@ -74,22 +81,22 @@ local current_cursor_pos=${CURSOR}
 
 # 次のラインの行頭に移動する
 cleverf-move-next-line() {
-# 次のラインに移動しない行末移動
-zle .vi-end-of-line
-# 次のラインに移動する行末移動
-zle .end-of-line
-# 前のラインに移動しない行頭移動
-zle .vi-beginning-of-line
+	# 次のラインに移動しない行末移動
+	zle .vi-end-of-line
+	# 次のラインに移動する行末移動
+	zle .end-of-line
+	# 前のラインに移動しない行頭移動
+	zle .vi-beginning-of-line
 }
 
 # 前のラインの行末に移動する
 cleverf-move-prev-line() {
-# 前のラインに移動しない行頭移動
-zle .vi-beginning-of-line
-# 前のラインに移動する行頭移動
-zle .beginning-of-line
-# 次のラインに移動しない行末移動
-zle .vi-end-of-line
+	# 前のラインに移動しない行頭移動
+	zle .vi-beginning-of-line
+	# 前のラインに移動する行頭移動
+	zle .beginning-of-line
+	# 次のラインに移動しない行末移動
+	zle .vi-end-of-line
 }
 
 cleverf-highlight-all() {
@@ -110,23 +117,23 @@ local cursor_position_char=${RBUFFER:0:1}
 }
 
 cleverf-get-length() {
-echo $(($(echo $1 | wc -m)-1))
+	echo $(($(echo $1 | wc -m)-1))
 }
 
 cleverf-next() {
-cleverf search_type_next
+	cleverf search_type_next
 }
 
 cleverf-prev() {
-cleverf search_type_prev
+	cleverf search_type_prev
 }
 
 cleverf-reset-highlight() {
-region_highlight=()
+	region_highlight=()
 }
 
 cleverf-highlight() {
-region_highlight+=("$1 $(($1+1)) bold,fg=red")
+	region_highlight+=("$1 $(($1+1)) bold,fg=red")
 }
 
 # 初期化
