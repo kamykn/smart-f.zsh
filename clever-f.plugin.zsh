2c1065a27eb8ba4ce7575b1ffb72a0f3c6137bb7
kamykn <to.kamykn@gmail.com>
2019-07-24 23:37:59 +0900

    refactoring and add highlight reset

--- clever-f.plugin.zsh
+++ clever-f.plugin.zsh
@@ -1,202 +1,245 @@
-local search_type_next=1
-local search_type_prev=2
-local search_type_mode=0
 
-clever-f() {
-	local search_type=$1
+if [[ ! -v SEARCH_TYPE_NEXT ]]; then
+    readonly SEARCH_TYPE_NEXT=1
+fi
 
-	if [[ ! -v prev_cursor_pos ]]; then
-		prev_cursor_pos=-1
-	fi
+if [[ ! -v SEARCH_TYPE_PREV ]]; then
+    readonly SEARCH_TYPE_PREV=2
+fi
 
-	clever-f-vi-find ${search_type} ${prev_cursor_pos}
+if [[ ! -v search_type_mode ]]; then
+	local search_type_mode=0
+fi
 
-	if [[ $? -ne 0 ]]; then
-		if [[ $tmp_prev_cursor_pos = $CURSOR ]]; then
-			clever-f-reset-highlight
-		fi
+_clever_f() {
+    local -i search_type=$1
 
-		return
-	fi
+    if [[ ! -v prev_cursor_pos ]]; then
+        prev_cursor_pos=-1
+    fi
 
-	# global
-	prev_cursor_pos=${CURSOR}
+    _clever_f_vi_find ${search_type} ${prev_cursor_pos}
 
-	clever-f-highlight-all
+    if [[ $? -ne 0 ]]; then
+        if [[ ${tmp_prev_cursor_pos} = ${CURSOR} ]]; then
+            _clever_f_reset_highlight
+        fi
+
+        return 1
+    fi
+
+    # global
+    prev_cursor_pos=${CURSOR}
+
+    _clever_f_highlight_all
+
+    return 0
 }
 
-clever-f-vi-find() {
-	local search_type=$1
-	local tmp_prev_cursor_pos=$2
-	local current_cursor_pos=${CURSOR}
+_clever_f_vi_find() {
+    local -i search_type=$1
+    local -i tmp_prev_cursor_pos=$2
+    local -i current_cursor_pos=${CURSOR}
 
-	if [[ $tmp_prev_cursor_pos -ne $current_cursor_pos ]]; then
-		clever-f-find $search_type
+    if [[ ${tmp_prev_cursor_pos} -ne ${current_cursor_pos} ]]; then
+        _clever_f_find ${search_type}
 
-		if [[ $? -eq 0 ]]; then
-			return 0
-		fi
-	fi
+        if [[ $? -eq 0 ]]; then
+            return 0
+        fi
+    fi
 
-	clever-f-repeat-find-loop $search_type
+    _clever_f_repeat_find_loop ${search_type}
 
-	if [[ $? -eq 0 ]]; then
-		return 0
-	fi
+    if [[ $? -eq 0 ]]; then
+        return 0
+    fi
 
-	return 1
+    return 1
 }
 
-clever-f-find() {
-	local search_type=$1
+_clever_f_find() {
+    local -i search_type=$1
 
-	if [[ $search_type -eq $search_type_next ]]; then
-		zle .vi-find-next-char
-	else
-		zle .vi-find-prev-char
-	fi
+    if [[ ${search_type} -eq ${SEARCH_TYPE_NEXT} ]]; then
+        zle .vi-find-next-char
+    else
+        zle .vi-find-prev-char
+    fi
 
-	if [[ $? -eq 0 ]]; then
-		search_type_mode=$search_type
-		return 0
-	fi
+    if [[ $? -eq 0 ]]; then
+        search_type_mode=${search_type}
+        return 0
+    fi
 
-	return 1
+    return 1
 }
 
-clever-f-repeat-find-loop() {
-	local search_type=$1
+_clever_f_repeat_find_loop() {
+    local -i search_type=$1
 
-	local current_line=$(echo "$LBUFFER" | wc -l | tr -d ' ')
-	local end_line=1
+    local -i current_line=$(echo "${LBUFFER}" | wc -l | tr -d ' ')
+    local -i end_line=1
 
-	if [[ $search_type -eq $search_type_next ]]; then
-		end_line=${BUFFERLINES}
-	else
-		end_line=1
-	fi
+    if [[ ${search_type} -eq ${SEARCH_TYPE_NEXT} ]]; then
+        end_line=${BUFFERLINES}
+    else
+        end_line=1
+    fi
 
-	local is_current_line=true
-	for line in $(seq ${current_line} ${end_line}); do
-		if [[ line -ne $current_line ]]; then
-			is_current_line=false
-		fi
+    local is_current_line=true
+    for line in $(seq ${current_line} ${end_line}); do
+        if [[ line -ne $current_line ]]; then
+            is_current_line=false
+        fi
 
-		clever-f-repeat-find $search_type $is_current_line
-		if [[ $? -eq 0 ]]; then
-			return 0
-		fi
-	done
+        _clever_f_repeat_find ${search_type} ${is_current_line}
+        if [[ $? -eq 0 ]]; then
+            return 0
+        fi
+    done
 
-	# 無かった場合にカーソル位置を戻す
-	CURSOR=$current_cursor_pos
+    # reset cursor position
+    CURSOR=${current_cursor_pos}
 
-	return 1
+    return 1
 }
 
-clever-f-repeat-find() {
-	local search_type=$1
-	local is_current_line=$2
+_clever_f_repeat_find() {
+    local -i search_type=$1
+    local is_current_line=$2
 
-	# 行移動
-	if ! "${is_current_line}"; then
-		if [[ $search_type -eq $search_type_next ]]; then
-			clever-f-move-next-line
-		else
-			clever-f-move-prev-line
-		fi
-	fi
+    # move line
+    if ! "${is_current_line}"; then
+        if [[ ${search_type} -eq ${SEARCH_TYPE_NEXT} ]]; then
+            _clever_f_move_next_line
+        else
+            _clever_f_move_prev_line
+        fi
+    fi
 
-	# repeatマッチ
-	if [[ $search_type_mode -eq $search_type ]]; then
-		zle .vi-repeat-find
-	else
-		# repeatを逆向きにする用
-		zle .vi-rev-repeat-find
-	fi
+    if [[ ${search_type_mode} -eq ${search_type} ]]; then
+		# for repeat
+        zle .vi-repeat-find
+    else
+        # for reverse repeat
+        zle .vi-rev-repeat-find
+    fi
 
-	if [[ $? -eq 0 ]]; then
-		if ! "${is_current_line}"; then
-			# 次/前の行マッチの1文字目がヒットできないので戻す
-			if [[ $search_type -eq $search_type_next ]]; then
-				zle .vi-rev-repeat-find
-			else
-				zle .vi-repeat-find
-			fi
-		fi
+    if [[ $? -eq 0 ]]; then
+        if ! "${is_current_line}"; then
+            # 次/前の行マッチの1文字目がヒットできないので戻す
+            if [[ ${search_type} -eq ${SEARCH_TYPE_NEXT} ]]; then
+                zle .vi-rev-repeat-find
+            else
+                zle .vi-repeat-find
+            fi
+        fi
 
-		return 0
-	fi
+        return 0
+    fi
 
-	return 1
+    return 1
 }
 
 # 次のラインの行頭に移動する
-clever-f-move-next-line() {
-	# 次のラインに移動しない行末移動
-	zle .vi-end-of-line
-	# 次のラインに移動する行末移動
-	zle .end-of-line
-	# 前のラインに移動しない行頭移動
-	zle .vi-beginning-of-line
+_clever_f_move_next_line() {
+    # 次のラインに移動しない行末移動
+    zle .vi-end-of-line
+    # 次のラインに移動する行末移動
+    zle .end-of-line
+    # 前のラインに移動しない行頭移動
+    zle .vi-beginning-of-line
+
+    return 0
 }
 
 # 前のラインの行末に移動する
-clever-f-move-prev-line() {
-	# 前のラインに移動しない行頭移動
-	zle .vi-beginning-of-line
-	# 前のラインに移動する行頭移動
-	zle .beginning-of-line
-	# 次のラインに移動しない行末移動
-	zle .vi-end-of-line
+_clever_f_move_prev_line() {
+    # 前のラインに移動しない行頭移動
+    zle .vi-beginning-of-line
+    # 前のラインに移動する行頭移動
+    zle .beginning-of-line
+    # 次のラインに移動しない行末移動
+    zle .vi-end-of-line
+
+    return 0
 }
 
-clever-f-highlight-all() {
-	local cursor_position_char=${RBUFFER:0:1}
+_clever_f_highlight_all() {
+    local cursor_position_char=${RBUFFER:0:1}
 
-	# 1文字ずつ
-	# echo で制御文字が消えるっぽい
-	local buffer_len=$(clever-f-get-length ${BUFFER})
-	local buffer_string=$(echo ${BUFFER})
+    # 1文字ずつ
+    # echo で制御文字が消えるっぽい
+    local -i buffer_len=$(_clever_f_get_length ${BUFFER})
+    local buffer_string=$(echo ${BUFFER})
 
-	for index in {0..$buffer_len}; do
-		local char=${buffer_string:${index}:1}
+    for index in {0..${buffer_len}}; do
+        local char=${buffer_string:${index}:1}
 
-		if [[ $char = $cursor_position_char ]]; then
-			clever-f-highlight ${index}
-		fi
+        if [[ ${char} = ${cursor_position_char} ]]; then
+            _clever_f_highlight ${index}
+        fi
+    done
+
+    return 0
+}
+
+_clever_f_get_length() {
+    echo $(($(echo $1 | wc -m)-1))
+    return 0
+}
+
+_clever_f_highlight() {
+    region_highlight+=("$1 $(($1+1)) bold,fg=red")
+    return 0
+}
+
+_clever_f_reset_highlight() {
+    region_highlight=()
+    return 0
+}
+
+clever_f_next() {
+    _clever_f ${SEARCH_TYPE_NEXT}
+    return 0
+}
+
+clever_f_prev() {
+    _clever_f ${SEARCH_TYPE_PREV}
+    return 0
+}
+
+_clever_f_bind_reset_highlight() {
+	local -U widgets_to_bind
+	widgets_to_bind=(${${(k)widgets}:#(.*|run-help|which-command|beep|set-local-history|yank|yank-pop)})
+
+	for cur_widget in $widgets_to_bind; do
+		case ${widgets[$cur_widget]:-""} in
+			builtin)
+				eval "_clever_f_call_widget_$cur_widget() {
+					builtin zle .$cur_widget
+					_clever_f_reset_highlight
+				}"
+
+				zle -N $cur_widget _clever_f_call_widget_$cur_widget
+		esac
 	done
 }
 
-clever-f-get-length() {
-	echo $(($(echo $1 | wc -m)-1))
-}
 
-clever-f-next() {
-	clever-f $search_type_next
-}
+# initialization
+_clever_f_reset_highlight
 
-clever-f-prev() {
-	clever-f $search_type_prev
-}
+# bind for reset highlight
+_clever_f_bind_reset_highlight
 
-clever-f-reset-highlight() {
-	region_highlight=()
-}
-
-clever-f-highlight() {
-	region_highlight+=("$1 $(($1+1)) bold,fg=red")
-}
-
-# 初期化
-clever-f-reset-highlight
-
-zle -N clever-f-next
-zle -N clever-f-prev
+zle -N clever_f_next
+zle -N clever_f_prev
 
 # for emacs mode
-bindkey "^X^F" clever-f-next
+bindkey "^X^F" clever_f_next
 
 # for vi mode
-bindkey -a 'f' clever-f-next
-bindkey -a 'F' clever-f-prev
+bindkey -a 'f' clever_f_next
+bindkey -a 'F' clever_f_prev
